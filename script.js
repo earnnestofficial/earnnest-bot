4c9c68b4be211c54cc8ad64ca39884731c72625c
earnnestofficial <usabdprosperity@gmail.com>
2026-08-10 01:25:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ console.log("Developer Mode:", DEV_MODE);
 console.log("User:", user);
 
 // ===== API =====
-
 const API_URL = "https://script.google.com/macros/s/AKfycby1PfOZ8dPri99Uwa2smMd-Nk66l29RC0w6jNH3HMeqQoKNs_G_WITUM71ar5mEmTePjg/exec";
 // ===== User =====
 let balance = 0;
